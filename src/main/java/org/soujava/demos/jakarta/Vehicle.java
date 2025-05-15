00ae71f44cf87585b81d115b98d0843a44f0634e
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-15 18:52:36 +0300

    feat: create vehicle
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/demos/jakarta/Vehicle.java
+++ src/main/java/org/soujava/demos/jakarta/Vehicle.java
@@ -5,17 +5,53 @@ import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
 import net.datafaker.Faker;
 
+import java.util.Objects;
+
 @Entity
 public class Vehicle {
 
     @Id
-    private String id;
+    private String vin;
     @Column
     private String model;
 
+    @Column
+    private String type;
 
+    @Column
+    private String transmission;
 
     public static Vehicle of(Faker faker) {
+        var vehicle = faker.vehicle();
+        Vehicle entity = new Vehicle();
+        entity.vin = vehicle.vin();
+        entity.model = vehicle.model();
+        entity.type = vehicle.carType();
+        entity.transmission = vehicle.transmission();
+        return entity;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vehicle vehicle = (Vehicle) o;
+        return Objects.equals(vin, vehicle.vin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(vin);
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vin='" + vin + '\'' +
+                ", model='" + model + '\'' +
+                ", type='" + type + '\'' +
+                ", transmission='" + transmission + '\'' +
+                '}';
     }
 }
