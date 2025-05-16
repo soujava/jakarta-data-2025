a24a88f1195efdb577c3fe90adadd7d9c3da8cba
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-16 07:31:18 +0200

    include query on garange
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/demos/jakarta/Garage.java
+++ src/main/java/org/soujava/demos/jakarta/Garage.java
@@ -1,9 +1,18 @@
 package org.soujava.demos.jakarta;
 
 import jakarta.data.repository.BasicRepository;
+import jakarta.data.repository.By;
+import jakarta.data.repository.Find;
 import jakarta.data.repository.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface Garage extends BasicRepository<Vehicle, String> {
+
+    List<Vehicle> findByTransmission(Transmission transmission);
+
+    @Find
+    List<Vehicle> findBy(@By("transmission") Transmission transmission);
 }
